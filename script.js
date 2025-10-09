9787c21e8b9ec7b690d70d8f5319af9ac5bbb010
itoihiroto <black.hari.shigesato@gmail.com>
2025-10-09 10:52:41 +0900

    Day5: ボタンにid追加

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("ページが開きました！");
+// ボタンを取得
+const button = document.getElementById("changeColorBtn");
+
+// ボタンがクリックされたときの動きを設定
+button.addEventListener("click", function() {
+  // ページ全体の背景色を変更
+  document.body.style.backgroundColor = getRandomColor();
+});
+
+// ランダムな色を作る関数
+function getRandomColor() {
+  const colors = ["#ffadad", "#ffd6a5", "#caffbf", "#9bf6ff", "#bdb2ff"];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
